1fc32c3b2d1c863249d1d8dde28d82363dbe3dc0
Piotr Kardasz <piotr.kardasz91@gmail.com>
2021-04-16 12:54:49 +0200

    Update Kernel namespace

--- tests/Application/Kernel.php
+++ tests/Application/Kernel.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Tests\Acme\SyliusExamplePlugin\Application;
+namespace Tests\BitBag\SyliusShippingExportPlugin\Application;
 
 use PSS\SymfonyMockerContainer\DependencyInjection\MockerContainer;
 use Sylius\Bundle\CoreBundle\Application\Kernel as SyliusKernel;
