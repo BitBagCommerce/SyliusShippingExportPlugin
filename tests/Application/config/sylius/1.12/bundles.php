d9c5532c704c8b76a87b0a0803171c9a8bef2d41
Rafal Malenta <rafal.malenta.extern@foodspring.com>
2023-03-21 07:58:55 +0100

    Upgraded most parts

---
+++ tests/Application/config/sylius/1.12/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    League\FlysystemBundle\FlysystemBundle::class => ['all'=> true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all'=> true],
+    Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true]
+];
