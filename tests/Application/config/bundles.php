737ac2bf49ad463c033e5929025b5c622c7bedaa
Rafal Malenta <rafal.malenta.extern@foodspring.com>
2023-03-21 07:58:55 +0100

    Upgraded most dependencies

--- tests/Application/config/bundles.php
+++ tests/Application/config/bundles.php
@@ -4,7 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Sylius\Bundle\OrderBundle\SyliusOrderBundle::class => ['all' => true],
@@ -53,5 +52,7 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Sylius\Bundle\ApiBundle\SyliusApiBundle::class => ['all' => true],
+//    League\FlysystemBundle\FlysystemBundle::class => ['all'=> true],
     SyliusLabs\DoctrineMigrationsExtraBundle\SyliusLabsDoctrineMigrationsExtraBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all'=> true],
 ];
---
+++ tests/Application/config/sylius/1.12/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    League\FlysystemBundle\FlysystemBundle::class => ['all'=> true],
+    Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true]
+];
