7211de085e0825e80167cd3bac9543eec6f43742
Kacper Kubacki <kacper.kubacki@bitbag.pl>
2025-03-20 11:06:04 +0100

    Upgrade to 1.14

--- tests/Application/config/sylius/1.14/bundles.php
+++ tests/Application/config/sylius/1.14/bundles.php
@@ -12,4 +12,5 @@ return [
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
     Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
+    Sylius\Abstraction\StateMachine\SyliusStateMachineAbstractionBundle::class => ['all' => true],
 ];
